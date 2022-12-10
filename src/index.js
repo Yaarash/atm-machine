e17bf6884d77f774f9bbfc85365b4ac01dde9723
Yaara Shoham <yaara.shoham@riseup.co.il>
2022-12-10 22:13:26 +0200

    Change response format

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const _ = require('lodash');
 
 const app = express();
 
 let map1 = _initBillsCoinsMap();
+
+const MAX_AMOUNT = 2000;
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
@@ -11,52 +14,97 @@ app.listen(3000, () => {
 app.get("/atm/withdrawal", (req, res, next) => {
   // const currency = req.params.currency;
   const withdrawlAmount = Number(req.query.amount);
-  const result = calculateResult(withdrawlAmount);
+  if (!_isValidWithdrawl(withdrawlAmount)) {
+    return sendResponse (res, 400, 'error: Max amount for a single withdrawal is 2000');
+  }
+  try {
+    const result = calculateResult(withdrawlAmount);
+  if (!result) {
+    return sendResponse (res, 409, 'error: Not enough money in the ATM, please try a different amount');
+  }
   console.log(map1);
-  return res.send(result);
+  return res.send({result});
+
+  } catch (error) {
+    console.error(error);
+    return sendResponse(res, 500, 'error: Too many coins to withdraw');
+  }
 });
 
 function _initBillsCoinsMap() {
   return {
     200: {'type': 'bill' ,'value': 200, 'amount': 7},
     100: {'type': 'bill','value': 100, 'amount': 4},
-    10: {'type': 'coin','value': 10, 'amount': 0},
+    20: {'type': 'bill','value': 20, 'amount': 1},
+    10: {'type': 'coin','value': 10, 'amount': 1},
     5: {'type': 'coin','value': 5, 'amount': 1},
     0.1: {'type': 'coin','value': 0.1, 'amount': 12},
     0.01: {'type': 'coin','value': 0.01, 'amount': 21}
   }
+
+}
+function sendResponse(res, statusCode = 200, message = '') {
+  return res.status(statusCode)
+    .send({ message});
+}
+function _isValidWithdrawl(withdrawlAmount) {
+  return withdrawlAmount <= MAX_AMOUNT;
 }
 
 function calculateResult(withdrawlAmount) {
   let remain = withdrawlAmount;
-  let usedBills = [];
+  const used = {
+    "bills": [],
+    "coins": []
+  }
+  const usedBills = {}
+  const usedCoins = {}
   let mapKeys = Object.keys(map1).sort((a, b) => {return b-a});
   for(let i = 0; i < mapKeys.length; i++) {
-    let tempBillsAmount = map1[mapKeys[i]].amount;
+    let billsAvailableAmount = map1[mapKeys[i]].amount;
     const bill = Number(mapKeys[i]);
-    if (tempBillsAmount > 0 && remain >= bill) {
+    if (billsAvailableAmount > 0 && remain >= bill) {
       const bankBill = Math.floor(remain / bill);
-      const maxBillsUsed = Math.min(tempBillsAmount, bankBill)
-      const objp = {type: map1[bill].type};
-      objp[bill] = maxBillsUsed;
-      usedBills.push(objp);
-      console.log(usedBills);
+      const maxBillsUsed = Math.min(billsAvailableAmount, bankBill)
+      if (map1[bill].type === 'bill') {
+        usedBills[bill] = maxBillsUsed;
+      }
+      if (map1[bill].type === 'coin') {
+        usedCoins[bill] = maxBillsUsed;
+      }
+      console.log(`usedBillsa: ${JSON.stringify(usedBills)}`);
+      console.log(`usedCoinsa: ${JSON.stringify(usedCoins)}`);
+      used.bills = [usedBills];
+      used.coins = [usedCoins];
+      console.log(`used!! ${JSON.stringify(used)}`);
 
-      tempBillsAmount -= maxBillsUsed;
+      billsAvailableAmount -= maxBillsUsed;
       remain -= maxBillsUsed * bill;
     }
   }
-  if (remain > 0){
-    return {
-      "msg": "Not enough bills and coins in ATM",
-      "bill": usedBills.filter(usedBill => usedBill.type === "bill"),
-      "coin": usedBills.filter(usedBill => usedBill.type === "coin")
-    }
+
+  if (_isTooManyCoins(used, mapKeys)){
+    throw new Error('TooMuchCoinsException');
   }
-  console.log(usedBills);
-  return {
-    "bill": usedBills.filter(usedBill => usedBill.type === "bill"),
-    "coin": usedBills.filter(usedBill => usedBill.type === "coin")
-  };
-  //TODO change response format
+  if (remain > 0){
+    return false;
+  }
+  return used;
 }
+
+function _isTooManyCoins(usedBills) {
+  let sum = 0;
+  const coins = Object.values(usedBills.coins[0] || 0)
+  coins.forEach(coin => sum += (coin || 0))
+  console.log(`sum is ${sum}`);
+  return sum > 50;
+}
+
+function getSortedMapKeys() {
+  return Object.keys(map1)
+    .sort((a, b) => {
+      return b - a;
+    });
+}
+
+
